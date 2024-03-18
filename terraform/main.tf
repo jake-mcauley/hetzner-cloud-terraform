f81ee005edd9fcd68d7c4c810ba240f61eb48b06
jake-mcauley <jakemcauley@hotmail.co.uk>
2024-03-18 23:34:25 +0000

    Remove docker install

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,23 +8,4 @@ resource "hcloud_server" "render_fm_server" {
   labels = {
     "role" = "render_fm_server"
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "apt-get update",
-      "apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
-      "apt-get update",
-      "apt-get install -y docker-ce",
-      "echo 'Docker CE installation completed successfully'"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = file(var.ssh_private_key_path)
-      host        = self.ipv4_address
-    }
-  }
 }
